c62b39acef04386cf6a4b6ac8a76b7e289cd6d5d
Quirine <Quirine.engbers@gmail.com>
2021-05-26 15:25:58 +0200

    Added serial communication. improved format.

--- scripts/lights.py
+++ scripts/lights.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 import rospy
+import serial
+import struct
 
 from assisted_cleaning_solution.msg import Task
 
-def lights_callback(msg):
-    print('Color light = ',msg.task)
+class task_subscriber:
+
+    def __init__(self):
+        self.microcontroller = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+        rospy.Subscriber('task', Task, self.lights_callback, queue_size=1)
+
+    def write_read(self, x):
+        arrayByte = bytearray(struct.pack('f', x))
+        self.microcontroller.write(arrayByte)
+        
+        print('Color light = ', x, arrayByte, struct.unpack('f', arrayByte))
+
+    def lights_callback(self, msg):
+        self.write_read(msg.task)
     
-def task_listener():
-    rospy.init_node('lights', anonymous=True)
-    rospy.Subscriber('task', Task, lights_callback)
+def main():
+    rospy.init_node('lights')
+    task_subscriber()
     rospy.spin()
 
 if __name__ == '__main__':
-    task_listener()
+    main()
