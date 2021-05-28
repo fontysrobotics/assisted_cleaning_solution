1336997cf5c92a10a62551ceca51026557a0dbea
Quirine <Quirine.engbers@gmail.com>
2021-05-28 14:59:00 +0200

    Separate file for serial communication.

--- scripts/lights.py
+++ scripts/lights.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 import rospy
-import serial
-import struct
 
 from assisted_cleaning_solution.msg import Task
 
 class task_subscriber:
 
     def __init__(self):
-        self.microcontroller = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
         rospy.Subscriber('task', Task, self.lights_callback, queue_size=1)
 
-    def write_read(self, x):
-        arrayByte = bytearray(struct.pack('f', x))
-        self.microcontroller.write(arrayByte)
-        
-        print('Color light = ', x, arrayByte, struct.unpack('f', arrayByte))
-
     def lights_callback(self, msg):
-        self.write_read(msg.task)
-    
+        print('Color light = ', msg.task)
+
 def main():
     rospy.init_node('lights')
     task_subscriber()
---
+++ scripts/microcontroller_communication.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+import serial
+
+from assisted_cleaning_solution.msg import Task
+
+class task_subscriber:
+
+    def __init__(self):
+        self.microcontroller = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.01)
+        rospy.Subscriber('task', Task, self.lights_callback, queue_size=1)
+        self.timer = rospy.Timer(rospy.Duration(0.01), self.timer_callback)
+
+    def write(self, x):
+        arrayByte = str(x) + '\n\r'
+        self.microcontroller.write(arrayByte.encode())
+        
+        print('Color light = ', x)
+
+    def lights_callback(self, msg):
+        self.write(msg.task)
+
+    def timer_callback(self, timer):
+        try:
+            data = self.microcontroller.readline()
+            print(data.decode())
+        except KeyboardInterrupt:
+            self.microcontroller.close()
+
+def main():
+    rospy.init_node('microcontroller_communication')
+    task_subscriber()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
