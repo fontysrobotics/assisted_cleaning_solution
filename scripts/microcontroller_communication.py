65cdecae064a3f492f667f3634c54db18e447486
Quirine <Quirine.engbers@gmail.com>
2021-06-29 15:21:36 +0200

    Message for sensor data from MCU

--- scripts/microcontroller_communication.py
+++ scripts/microcontroller_communication.py
@@ -2,19 +2,26 @@
 import rospy
 import serial
 
-from assisted_cleaning_solution.msg import Task
+from assisted_cleaning_solution.msg import Task, Sensor
 
 class task_subscriber:
 
     def __init__(self):
         self.microcontroller = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.01)
+        self.sensor_publisher = rospy.Publisher('sensor', Sensor, queue_size=10)
 
-        rospy.Subscriber('task', Task, self.lights_callback, queue_size=1)
+        self.task_subsriber = rospy.Subscriber('task', Task, self.lights_callback, queue_size=1)
         self.timer = rospy.Timer(rospy.Duration(0.01), self.timer_callback)
 
-    def write(self, x):
-        arrayByte = str(x) + '\n\r'
-        self.microcontroller.write(arrayByte.encode())
+    def publish_sensor(self, ir_back, ir_front, sonar1, sonar2):
+        sensor = Sensor()
+
+        sensor.ir_back = ir_back
+        sensor.ir_front = ir_front
+        sensor.sonar1 = sonar1
+        sensor.sonar2 = sonar2
+
+        self.sensor_publisher.publish(sensor)
 
     def lights_callback(self, msg):
         self.write(msg.task)
@@ -25,9 +32,15 @@ class task_subscriber:
             print(data.decode())
             self.microcontroller.flushInput()
 
+            self.publish_sensor(1,2,3,4)
+
         except KeyboardInterrupt:
             self.microcontroller.close()
 
+    def write(self, x):
+        arrayByte = str(x) + '\n\r'
+        self.microcontroller.write(arrayByte.encode())
+            
 def main():
     rospy.init_node('microcontroller_communication')
     task_subscriber()
